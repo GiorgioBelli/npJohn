19b24057ab0a734f27be10ffe112542cd5d3c2c6
AlessioLuciani <alessio99.luciani@gmail.com>
2020-01-05 12:46:39 +0100

    Add new program launcher and Makefile

---
+++ src/input-handler/input_handler.h
@@ -0,0 +1,38 @@
+#ifndef STDIO_H
+    #include <stdio.h>
+#endif
+
+#ifndef STDLIB_H
+    #include <stdlib.h>
+#endif
+
+#ifndef STRING_H
+    #include <string.h>
+#endif
+
+#ifndef PASSWORD_H
+    #include "../password/password.h"
+#endif
+
+#ifndef DIGEST_H
+    #include "../hash/digest.h"
+#endif
+
+#define HANDLE_H
+#define MAXCHAR 1000
+
+typedef struct passwordList{  
+    Password  obj; 
+    struct passwordList *next; 
+}PasswordList;
+
+typedef struct dictList{
+    char* word;
+    struct dictList* next;
+}DictList;
+
+void addElement(PasswordList** head, Password* obj);
+PasswordList* createStruct(char* fileName);
+DictList* importFileDict(char* fileName);
+
+#include "input_handler.c"
